9268fcf29e1388129d8e14e0968129a86413cabc
weiqi11111 <153408898+weiqi11111@users.noreply.github.com>
2025-06-24 21:17:23 +0800

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('Expires', '0');
+    
     const token = req.cookies.Authorization;
 
     if (!token) {
